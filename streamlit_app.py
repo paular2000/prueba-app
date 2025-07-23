9e3718f05c7846d240bb892ac859210e7cbd0149
Paula Rodríguez <paular2000@uma.es>
2025-07-23 13:00:34 +0200

    hola

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My nueva prueba")
+st.title("Formulario de registro de pacientes")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+with st.form(key="registro_form"):
+    nombre = st.text_input("Nombre")
+    edad = st.number_input("Edad",
+                            min_value= 0,
+                            max_value= 100)
+    profesion = st.text_input("Profesión")
